46c211292f91b933ae16a25356c28c13a391ce60
marzaguina <133380267+marzaguina@users.noreply.github.com>
2023-05-20 19:05:08 +0200

    Update new.c

--- new.c
+++ new.c
@@ -7,69 +7,85 @@
 #define ANSI_COLOR_RESET "\033[0m"
 
 int main() {
-    FILE* f = NULL;                  // programme pour l'intro i 
-    f = fopen("intro+1er choix.txt", "r+");
+    FILE* f = NULL;                  // programme pour l'intro i
+    f = fopen("intro.txt", "r+");
     if (f == NULL) {
-        printf("Erreur lors de l'ouverture du fichier suite choix 1.txt\n");
+        printf("Erreur lors de l'ouverture du fichier\n");
         exit(1);
     }
 
     char c;
     while ((c = fgetc(f)) != EOF) {
         printf(ANSI_COLOR_BLUE "%c" ANSI_COLOR_RESET, c);
-        fflush(stdout);  // Vider le tampon de sortie pour afficher le caractère immédiatement
+        /*fflush(stdout);  // Vider le tampon de sortie pour afficher le caractère immédiatement
         usleep(50000);  // Pause de 50 000 microsecondes (0,05 seconde)
-    }
+    */}
 
-    int a;                   // programme pour i_c1||c2 
+    int a;                   // programme pour i_c1||c2
     scanf("%d", &a);
 
-do{
+while(a!=1 && a!=2){
     printf("tapez 1 ou 2\n");
     scanf("%d", &a);
-}while(a!=1 && a!=2);
+}
 
-    
+   
 
     FILE* f1 = NULL;
     if (a == 1) {
-        f1 = fopen("i_c1.txt", "r+");
+        f1 = fopen("ic1.txt", "r+");
     } else if (a == 2) {
-        f1 = fopen("i_c2.txt", "r+");
+        f1 = fopen("ic2.txt", "r+");
     }
 
 
     char c1;
     while ((c1 = fgetc(f1)) != EOF) {
         printf(ANSI_COLOR_BLUE "%c" ANSI_COLOR_RESET, c1);
-        fflush(stdout);  // Vider le tampon de sortie pour afficher le caractère immédiatement
-        usleep(50000);  // Pause de 50 000 microsecondes (0,05 seconde)
+        /*fflush(stdout);  // Permet d'afficher lettre par lettre avec un délai de 0,05susleep(50000);
+    usleep(50000);*/
+       
     }
 
 
 
-int a1;                                    //programme pour i_c1_c1||c2 
-
+int a1;                                    //programme pour i_c1_c1||c2
 scanf("%d", &a1);
-do{
+
+while(a1!=1 && a1!=2){
     printf("tapez 1 ou 2\n");
     scanf("%d", &a1);
-}while(a1!=1 && a1!=2);
+}
 
 FILE* f2 = NULL;
+    if(a==1){
     if (a1 == 1) {
-        f2 = fopen("i_c1_c1.txt", "r+");
+        f2 = fopen("ic1_c1.txt", "r+");
     } else if (a1 == 2) {
-        f2 = fopen("i_c2_c2.txt", "r+");
+        f2 = fopen("ic1_c2.txt", "r+");
     }
-
+}
+else if(a==2){
+    if (a1 == 1) {
+        f2 = fopen("ic2_c1.txt", "r+");
+    } else if (a1 == 2) {
+        f2 = fopen("ic2_c2.txt", "r+");
+    }
+}
 
     char c2;
     while ((c2 = fgetc(f2)) != EOF) {
         printf(ANSI_COLOR_BLUE "%c" ANSI_COLOR_RESET, c2);
-        fflush(stdout);  // Vider le tampon de sortie pour afficher le caractère immédiatement
+        /*fflush(stdout);  // Vider le tampon de sortie pour afficher le caractère immédiatement
         usleep(50000);  // Pause de 50 000 microsecondes (0,05 seconde)
-    }
+*/    }
+
+
+
+
+
+
+
 
 
 
