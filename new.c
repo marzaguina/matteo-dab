b03a6e8c0371096491678e7b4a1f475fa40600de
marzaguina <133380267+marzaguina@users.noreply.github.com>
2023-05-21 17:09:18 +0200

    Update new.c

--- new.c
+++ new.c
@@ -21,7 +21,7 @@ int main() {
         usleep(50000);  // Pause de 50 000 microsecondes (0,05 seconde)
     */}
 
-    int a;                   // programme pour i_c1||c2
+    int a;                   
     scanf("%d", &a);
 
 while(a!=1 && a!=2){
@@ -31,7 +31,7 @@ while(a!=1 && a!=2){
 
    
 
-    FILE* f1 = NULL;
+    FILE* f1 = NULL;                                  // programme pour i_c1||c2
     if (a == 1) {
         f1 = fopen("ic1.txt", "r+");
     } else if (a == 2) {
@@ -81,14 +81,68 @@ else if(a==2){
 */    }
 
 
+int a2;                                    //programme pour i_c1_c1||c2
+scanf("%d", &a2);
+
+while(a2!=1 && a2!=2){
+    printf("tapez 1 ou 2\n");
+    scanf("%d", &a2);
+}
+
+FILE* f3 = NULL;
+if(a==1){
+   
+    if (a1 == 1) {
+        if(a2==1){
+        f3 = fopen("ic1_c1_c1.txt", "r+");
+        }
+        else if(a2==2){
+            
+        f3 = fopen("ic1_c1_c2.txt", "r+");
+            
+        }
+        
+    } else if (a1 == 2) {
+        if(a2==1){
+        f3 = fopen("ic1_c2_c1.txt", "r+");
+    }
+      else if(a2==2){
+    f3 = fopen("ic1_c2_c2.txt", "r+");
+}
+        
+    }
+}
+else if(a==2){
+    if (a1 == 1) {
+        if(a2==1){
+            f3 = fopen("ic2_c1_c1.txt", "r+");
+            
+        }
+        else if(a2==2){
+            f3 = fopen("ic2_c1_c2.txt", "r+");
+            
+        }
+           
+        
+    } else if(a1 == 2) {
+        if(a2==1){
+        f3 = fopen("ic2_c2_c1.txt", "r+");    
+        }
+        else if(a2==2){
+            
+        f3 = fopen("ic2_c2_c2.txt", "r+");
+            
+        }
+    }
+}
 
 
+    char c3;
+    while ((c3 = fgetc(f3)) != EOF) {
+        printf(ANSI_COLOR_BLUE "%c" ANSI_COLOR_RESET, c3);
+        /*fflush(stdout);  // Vider le tampon de sortie pour afficher le caractère immédiatement
+        usleep(50000);  // Pause de 50 000 microsecondes (0,05 seconde)
+*/    }
 
-
-
-
-
-
-
-    return 0;
+return 0;
 }
